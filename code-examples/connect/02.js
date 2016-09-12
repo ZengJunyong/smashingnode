828ab984fc5c21dffccb6cd947b22840c0549b82
Junyong <zengjunyong@gmail.com>
2016-09-12 16:38:32 +0800

    serve-static

---
+++ code-examples/connect/02.js
@@ -0,0 +1,16 @@
+// https://www.npmjs.com/package/serve-static
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder 
+var serve = serveStatic('introduction/connect/website', {'index': ['index.html', 'index.htm']})
+
+// Create server 
+var server = http.createServer(function(req, res) {
+    console.log(req.headers); 
+    serve(req, res, finalhandler(req, res))
+})
+
+// Listen 
+server.listen(3000)
